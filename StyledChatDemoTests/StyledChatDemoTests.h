7835b8bf61736b21f0c40f7501c5434b4392932a
xiaohaibo <xiao_hb@qq.com>
2013-02-23 23:46:00 +0800

    add

--- StyledChatDemoTests/StyledChatDemoTests.h
+++ StyledChatDemoTests/StyledChatDemoTests.h
@@ -2,8 +2,8 @@
 //  StyledChatDemoTests.h
 //  StyledChatDemoTests
 //
-//  Created by xiaohaibo on 13-2-23.
-//  Copyright (c) 2013年 xiaohaibo. All rights reserved.
+//  Created by xiao haibo on 13-2-23.
+//  Copyright (c) 2013年 xiao haibo. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
